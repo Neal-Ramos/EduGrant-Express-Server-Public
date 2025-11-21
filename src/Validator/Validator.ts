ea061ab8d126ad0cfc69027040d3010ceda137e5
Neal_Eleazar_J_Ramos <nealramos72@gmail.com>
2025-11-21 21:32:05 +0800

    Bug Fixed

--- src/Validator/Validator.ts
+++ src/Validator/Validator.ts
@@ -20,53 +20,53 @@ export const validate =
   };
 
 export const toFloat = () => {
-  return z.union([z.string(), z.number()]).transform((val, cxt) => {
+  return z.union([z.string(), z.number()]).transform((val, ctx) => {
     const n = typeof val === 'string' ? Number(val) : val;
     if (Number.isNaN(n)) {
-      cxt.addIssue({ code: 'custom', message: 'Invalid Float!' });
+      ctx.addIssue({ code: 'custom', message: 'Invalid Float!' });
       return z.NEVER;
     }
     return n;
   });
 };
 export const toInt = () => {
-  return z.union([z.string(), z.number()]).transform((val, cxt) => {
+  return z.union([z.string(), z.number()]).transform((val, ctx) => {
     const n = typeof val === 'string' ? Number(val) : val;
     if (!Number.isInteger(n)) {
-      cxt.addIssue({ code: 'custom', message: 'Invalid Integer!' });
+      ctx.addIssue({ code: 'custom', message: 'Invalid Integer!' });
       return z.NEVER;
     }
     return n;
   });
 };
 export const toJSON = () => {
-  return z.string().transform((val, cxt) => {
+  return z.string().transform((val, ctx) => {
     try {
       const parsed = JSON.parse(val);
       return parsed;
     } catch (error) {
-      cxt.addIssue({ code: 'custom', message: 'Invalid JSON Format!' });
+      ctx.addIssue({ code: 'custom', message: 'Invalid JSON Format!' });
       return z.NEVER;
     }
   });
 };
 export const toDate = () => {
-  return z.union([z.string(), z.date(), z.number()]).transform((val, cxt) => {
+  return z.union([z.string(), z.date(), z.number()]).transform((val, ctx) => {
     const d = new Date(val);
     if (isNaN(d.getTime())) {
-      cxt.addIssue({ code: 'custom', message: 'Invalid Time Format!' });
+      ctx.addIssue({ code: 'custom', message: 'Invalid Time Format!' });
       return z.NEVER;
     }
     return d;
   });
 };
 export const toBoolean = () => {
-  return z.union([z.string(), z.boolean()]).transform((val, cxt) => {
-    if (typeof val === 'boolean') return val;
-    if (val !== 'true' && val !== 'false') {
-      cxt.addIssue({ code: 'custom', message: 'value must be "true" || "false"!' });
+  return z.union([z.string(), z.boolean()]).transform((val, ctx) => {
+    if (typeof val === "boolean") return val;
+    if (val !== "true" && val !== "false") {
+      ctx.addIssue({ code: 'custom', message: 'value must be "true" || "false"!' });
       return z.NEVER;
     }
-    return val === 'false';
+    return val === "true";
   });
 };
